ceb6a004425ee754e9082611ee4d5282f086db09
= <=>
2020-11-16 13:41:43 +0100

    Added Class1

---
+++ Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VcsProject
+{
+    class Class1
+    {
+        void dosmth()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
